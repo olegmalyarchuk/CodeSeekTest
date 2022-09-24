01d34b8dd805d1df2fe93d5bda413a19177130f2
olegmalyarchuk <olegmalyarchuk7@gmail.com>
2022-09-24 15:19:17 +0300

    Added Player&Team DTOs

---
+++ FootballManager/src/main/java/com/example/footballmanager/dto/PlayerDTO.java
@@ -0,0 +1,41 @@
+package com.example.footballmanager.dto;
+
+import com.example.footballmanager.validation.AdvancedInfo;
+import com.example.footballmanager.validation.BasicInfo;
+import com.example.footballmanager.validation.PositiveDigit;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PlayerDTO {
+  @PositiveDigit
+  @NotNull(message = "{validation.id.not_null}")
+  private int id;
+
+  @Pattern(regexp = "^[A-ZА-Я][\\p{Alpha}А-Яа-я\\s-]{1,25}", groups = BasicInfo.class)
+  @NotBlank(message = "{validation.name.not_null}", groups = AdvancedInfo.class)
+  private String name;
+
+  @Pattern(regexp = "^[A-ZА-Я][\\p{Alpha}А-Яа-я\\s-]{1,25}", groups = BasicInfo.class)
+  @NotBlank(message = "{validation.name.not_null}", groups = AdvancedInfo.class)
+  private String surname;
+
+  @PositiveDigit
+  @NotNull(message = "{validation.id.not_null}")
+  private int age;
+
+  @PositiveDigit
+  @NotNull(message = "{validation.id.not_null}")
+  private int experienceMonths;
+
+  @PositiveDigit
+  @NotNull(message = "{validation.id.not_null}")
+  private int teamId;
+}
