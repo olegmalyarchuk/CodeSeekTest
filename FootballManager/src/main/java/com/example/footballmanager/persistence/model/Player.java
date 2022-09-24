366c0adc82f853410e68ede8faf9bacec2395e45
olegmalyarchuk <olegmalyarchuk7@gmail.com>
2022-09-24 15:11:20 +0300

    Added Swagger for Player&Team model

--- FootballManager/src/main/java/com/example/footballmanager/persistence/model/Player.java
+++ FootballManager/src/main/java/com/example/footballmanager/persistence/model/Player.java
@@ -1,5 +1,6 @@
 package com.example.footballmanager.persistence.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,21 +18,27 @@ public class Player {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Player ID")
   private int id;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Player Name")
   private String name;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Player Surname")
   private String surname;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Player age")
   private int age;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Player months of experience")
   private int experienceMonths;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "teamId", referencedColumnName = "id")
+  @ApiModelProperty(notes = "Player Team")
   private Team team;
 }
--- FootballManager/src/main/java/com/example/footballmanager/persistence/model/Team.java
+++ FootballManager/src/main/java/com/example/footballmanager/persistence/model/Team.java
@@ -1,5 +1,6 @@
 package com.example.footballmanager.persistence.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,18 +18,23 @@ import java.util.List;
 public class Team {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @ApiModelProperty(notes = "Team ID")
   private int id;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Team Name")
   private String name;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Team Wallet")
   private int wallet;
 
   @Column(nullable = false)
+  @ApiModelProperty(notes = "Team commision")
   private int commisionTeam;
 
   @OneToMany(mappedBy = "team", fetch = FetchType.EAGER)
   @Builder.Default
+  @ApiModelProperty(notes = "Team Players")
   private List<Player> players = new ArrayList<>();
 }
