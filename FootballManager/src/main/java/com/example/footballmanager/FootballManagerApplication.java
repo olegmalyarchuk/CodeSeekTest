764a095be04f3a335002f5e9b0aa511c57ecbe64
olegmalyarchuk <olegmalyarchuk7@gmail.com>
2022-09-24 14:03:10 +0300

    Added Logging Interceptor

--- FootballManager/src/main/java/com/example/footballmanager/FootballManagerApplication.java
+++ FootballManager/src/main/java/com/example/footballmanager/FootballManagerApplication.java
@@ -1,13 +1,20 @@
 package com.example.footballmanager;
 
+import com.example.footballmanager.interceptor.LoggingInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FootballManagerApplication {
+public class FootballManagerApplication implements WebMvcConfigurer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FootballManagerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(FootballManagerApplication.class, args);
+  }
 
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LoggingInterceptor());
+  }
 }
