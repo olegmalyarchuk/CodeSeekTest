5bb7539d460aeb23ef902f9ca2990c5111e4be73
olegmalyarchuk <olegmalyarchuk7@gmail.com>
2022-09-24 22:25:09 +0300

    Added @Mapper for mapstruct

--- FootballManager/src/main/java/com/example/footballmanager/mapper/PlayerMapper.java
+++ FootballManager/src/main/java/com/example/footballmanager/mapper/PlayerMapper.java
@@ -3,13 +3,16 @@ package com.example.footballmanager.mapper;
 import com.example.footballmanager.dto.PlayerDTO;
 import com.example.footballmanager.persistence.model.Player;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface PlayerMapper {
   PlayerMapper INSTANCE = Mappers.getMapper(PlayerMapper.class);
 
+  @Mapping(source = "team.id", target = "teamId")
   PlayerDTO toDTO(Player model);
 
+  @Mapping(source = "teamId", target = "team.id")
   Player toModel(PlayerDTO dto);
 }
--- FootballManager/src/main/java/com/example/footballmanager/mapper/TeamMapper.java
+++ FootballManager/src/main/java/com/example/footballmanager/mapper/TeamMapper.java
@@ -1,13 +1,16 @@
 package com.example.footballmanager.mapper;
 
-import com.example.footballmanager.dto.PlayerDTO;
 import com.example.footballmanager.dto.TeamDTO;
 import com.example.footballmanager.persistence.model.Player;
 import com.example.footballmanager.persistence.model.Team;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = PlayerMapper.class)
 public interface TeamMapper {
   TeamMapper INSTANCE = Mappers.getMapper(TeamMapper.class);
 
